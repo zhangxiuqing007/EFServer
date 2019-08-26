4aba8833063dfa81efb0e2b675a1ff921bc009e2
zhangxiuqing007 <690313521@qq.com>
2019-08-26 22:12:10 +0800

    增加主题、话题定义
    结构体的定义，全都改用引用。

--- forum/dataStruct.go
+++ forum/dataStruct.go
@@ -1,22 +1,42 @@
 package forum
 
+//Theme 主题
+type Theme struct {
+	ID   int64
+	Name string
+}
+
+//Topic 话题
+type Topic struct {
+	ID          int64
+	Name        string
+	Explain     string
+	State       int
+	CreatedTime int64
+	CreateUser  *User
+	PermitUser  *User
+}
+
 //Post 帖子
 type Post struct {
 	PostBase
 	Title    string
-	Comments []Comment
+	Theme    *Theme
+	Topics   []*Topic
+	Comments []*Comment
 }
 
 //Comment 评论
 type Comment struct {
 	PostBase
-	PostID int64
+	Post *Post
 }
 
 // PostBase 基础结构
 type PostBase struct {
-	ID     int64
-	UserID int64
+	ID int64
+
+	User *User
 
 	Content string
 
@@ -40,8 +60,7 @@ type User struct {
 
 	SignUpTime int64
 
-	UserType int
-
+	UserType  int
 	UserState int
 }
 
@@ -50,16 +69,24 @@ const (
 	UserTypeAdministrator = iota
 	//UserTypeNormalUser 用户类型：普通用户
 	UserTypeNormalUser
+
 	//UserStateNormal 用户状态：正常
 	UserStateNormal = iota
 	//UserStateLock 用户账号：锁定
 	UserStateLock
+
 	//PostStateNormal 帖子状态：正常
 	PostStateNormal = iota
 	//PostStateLock 帖子状态：锁定
 	PostStateLock
+
 	//CmtStateNormal 评论状态：正常
 	CmtStateNormal = iota
 	//CmtStateLock 评论状态：锁定
 	CmtStateLock
+
+	//TopicStateNormal 话题状态：正常
+	TopicStateNormal = iota
+	//TopicStateLock 话题状态：锁定
+	TopicStateLock
 )
